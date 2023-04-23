880a357d2928f8f39d640e5c297f54d59c532a3d
Greg Detre <greg@gregdetre.co.uk>
2023-04-23 05:18:43 +0100

    Scraping using Readability.js

--- pages/api/APIagent.js
+++ pages/api/APIagent.js
@@ -1,21 +1,27 @@
 import { NextResponse } from 'next/server';
-import { getQuestions } from '@/utils/questions'; 
+import { getQuestions } from '@/utils/questions';
+import { getArticleText } from '@/utils/scraper';
+const fs = require('fs');
+
 
 export default async function handler(req, res) {
     try {
-    const { input } = await req.body
-    
-    console.log("input" + input);
+        const { input } = await req.body
 
-    const response = await getQuestions(input);
-    console.log("response" + response);
-    
-    res.status(200).json({ output: response});
+        console.log("input" + input);
+
+        var article = await getArticleText(input);
+        article = article.trim()
+        // GPT-3.5 can handle 4000 tokens, i.e. 3000 words, i.e. 18000 characters
+        article = article.slice(0, 15000);
+        // console.log(article);
+        const response = await getQuestions(article);
+        console.log("response" + response);
+        res.status(200).json({ output: response });
 
     } catch (error) {
         console.log(error);
     }
-    
 
 };
 
