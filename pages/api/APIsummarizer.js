ed82b38cde727681871ed2022e24a13cccf17c2c
caeochoa <caeochoa@gmail.com>
2023-04-23 14:42:03 +0100

    debugging summarizer api

--- pages/api/APIsummarizer.js
+++ pages/api/APIsummarizer.js
@@ -4,8 +4,9 @@ export default async function handler(req, res) {
 
     try {
         const { input } = await req.body
+        console.log('input' + (await input))
 
-        const num_words = input.split(" ").length
+        const num_words = (await input).split(" ").length
         if (num_words > 100) {
             const summary = await summarize(input);
 
@@ -17,7 +18,7 @@ export default async function handler(req, res) {
 
 
     } catch (error) {
-        // console.log(error);
+        console.log(error);
         console.log('error in APIsummarizer')
     }
 
