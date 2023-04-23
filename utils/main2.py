14f23dddcb7858dbd13085aac4a1f55795115386
Greg Detre <greg@gregdetre.co.uk>
2023-04-23 02:04:46 +0100

    Playing with Python prompts

--- utils/main2.py
+++ utils/main2.py
@@ -2,9 +2,11 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 import openai
-# import os
+import os
+import prompts
 
-# os.environ["OPENAI_API_KEY"] = open("openai.SECRET", "r").read().strip()
+
+os.environ["OPENAI_API_KEY"] = open("openai.SECRET", "r").read().strip()
 openai.api_key = open("openai.SECRET", "r").read().strip()
 
 
@@ -34,8 +36,21 @@ def call_gpt4(prompt: str):
     print(out)
 
 
+def call_langchain(text):
+    llm = OpenAI(temperature=0.9)
+    prompt = PromptTemplate(
+        input_variables=["text"],
+        template="Your job is to create 5 unique questions based on the following text in order to test a student: {text}",
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    print(chain.run(text))
+
+
 if __name__ == "__main__":
-    call_gpt4("my name is")
-
-
+    text = open("texts/Testing effect.markdown", "r").read().strip()
+    text = text[:2000]
+    prompt = prompts.questioner.format(text=text)
+    # print(prompt)
+    # call_gpt4("prompt")
+    call_langchain(text)
     
---
+++ utils/prompts.py
@@ -0,0 +1,24 @@
+questioner_json = """
+
+Write 5 unique questions based on the following text in order to test a student. Output in json.
+
+----
+Text:
+
+{text}
+
+----
+Questions:
+
+{{
+    "questions": [
+"""
+
+
+questioner = """
+
+Write 5 unique questions based on the following text in order to test a student. Output in json.
+
+{text}
+"""
+
